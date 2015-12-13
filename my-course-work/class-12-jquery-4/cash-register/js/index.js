63bd38a3b380442ff17632cd8ab2a39ac744cc74
Beastington <richpean@richards-MBP.home>
2015-12-12 21:09:22 -0500

    finished cash register

---
+++ my-course-work/class-12-jquery-4/cash-register/js/index.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){
+	var total = 0
+
+	$('#entry').submit(function(event) {
+
+		event.preventDefault()
+		var newCost = $('#newEntry').val()
+		newCost = parseFloat(newCost)
+		//cost = parseFloat(cost)
+		total += newCost
+		var addtoreceipt = '<tr><td></td><td>' + '$' + newCost + '</td></tr>'
+		$('#entries').append(addtoreceipt)
+		$('')
+		$('#total').text('$' + total);
+		$('#newEntry').val('')
+	})
+
+})
+
+
+
+
+/*$(document).ready(function(){
+	$('#entry').submit(function(event) {
+		event.preventDefault()
+		var cost = $('#newEntry').val()
+		$('tbody#entries').text('<tr>' + cost + '</tr>')
+
+		//alert('<tr>' + cost + '</tr>')
+
+	})
+
+
+})*/
---
+++ my-course-work/class-13-jquery-5/form-test/app.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+	$('#myForm').submit(function(event) {
+		event.preventDefault()
+		var name = $('#name-input').val()
+		$('#target').text(name)
+	})
+})
